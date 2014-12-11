7897fcf53a46a2499dc9ad3cf4a34ea0e5fdc2f1
Ricardo Franco <ricardo.krieg@gmail.com>
2014-12-11 20:39:25 -0300

    auto add **init** attribute
    automatically adding **init** attribute based on **model** attribute

--- lib/simple_form_angular/form_builder.rb
+++ lib/simple_form_angular/form_builder.rb
@@ -7,6 +7,10 @@ module SimpleFormAngular
     end
 
     def input_with_angular(attribute_name, options = {}, &block)
+      if ng_options.key?(:model) && !ng_options.key?(:init) && object.respond_to?(ng_options[:model])
+        ng_options[:init] = "#{ng_options[:model]}='#{object.send(ng_options[:model])}'"
+      end
+      
       if ng_options = options.delete(:ng)
         options[:input_html] ||= {}
         options[:input_html].merge! SimpleFormAngular.build_angular_options(ng_options)
